8a3851434e306f5f388b3541702f9fbcbfc756f1
Sayafdine Said <musikid@outlook.com>
2023-04-22 16:51:38 +0200

    ci: remove args

--- bundle.py
+++ bundle.py
@@ -4,11 +4,7 @@ import os
 
 specfile = sys.argv[1] if len(sys.argv) > 1 else "routology.spec"
 
-match os.name:
-    case "nt":
-        args = ["--uac-admin"]
-    case _:
-        args = []
+args = []
 
 sub = subprocess.run(["pyinstaller", *args, specfile])
 
